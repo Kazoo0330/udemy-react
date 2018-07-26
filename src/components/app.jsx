3775a89cb5bc96370eae3b9ce94435275c5ee05a
kazoo <kazuki_saito@diveintocode.jp>
2018-07-26 22:02:31 +0900

    build: greeting.jsx deleted and modified app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,34 +1,15 @@
 import React, { Component } from 'react';
 
-import Greeting from './greeting';
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'kazooooo',
     };
-  }
-
-  handleNameChange(e) {
-    console.log(name);
-    this.setState({ name: e.target.value });
-  }
-
-  handleKazooClicked() {
-    this.setState({ name: 'KAZOO' });
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={ e => this.handleNameChange(e)}
-        />
-        <button onClick={() => this.handleKazooClicked()}>I'm KAZOO</button>
-        <Greeting name={this.state.name} />
       </div>
     );
   }
