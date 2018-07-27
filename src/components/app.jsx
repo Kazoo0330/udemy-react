2b7b242cde51569c5c1bde0a91044f117fbc419e
kazoo <kazuki_saito@diveintocode.jp>
2018-07-27 13:56:16 +0900

    feat: section3 lecture22 paused

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import SearchForm from './SearchForm';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -7,9 +9,15 @@ class App extends Component {
     };
   }
 
+  handlePlaceSubmit(place) {
+  console.log(place);
+  }
+
   render() {
     return (
       <div>
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handleSubmit(place)} />
       </div>
     );
   }
